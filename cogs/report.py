b50d0d8c4c506dab0fcd0796f43ad9b5729ff65d
LoliChann <l.otaku.roux@gmail.com>
2022-03-02 11:08:44 +0100

    important things
    Load, unload & reload
    cogs
    report commands

---
+++ cogs/report.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+import json
+
+class Report(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def report(self, ctx, *, reason):
+        
+        with open (f"./report.json", "r") as f:
+            data = json.load(f)
+            register = data["register"]
+            
+        if not str(ctx.author) in register:
+            data.get("register", {}).append(str(ctx.author))
+            data[f"{ctx.author}"] = [reason]
+            with open (f"./report.json", "w") as t:
+                json.dump(data, t)
+        else:
+            data.get(f"{ctx.author}", {}).append(reason)
+            with open (f"./report.json", "w") as t:
+                json.dump(data, t)
+            
+        await ctx.reply("Report réalisé avec succès !")
+        
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def reportlist(self, ctx):
+        with open (f"./report.json", "r") as f:
+            data = json.load(f)
+            register = data["register"]
+        
+        embed = discord.Embed(title="Report list", description="Liste de tous les reports", color=discord.Color.from_rgb(17, 100, 20))
+        classement = 1
+
+        for userl in register:
+            with open (f"./report.json", "r") as f:
+                data = json.load(f)
+                user = data[f"{userl}"]  
+
+            for report in user:
+                print(report)
+                embed.add_field(name=f"{classement}. {userl}", value=f"report : **{report}**", inline=False)
+                classement = classement + 1
+
+
+
+        await ctx.reply(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Report(bot))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import json
 from discord.ext import commands
+import os
 
 with open ("data.json", "r") as f:
     data = json.load(f)
@@ -9,9 +10,7 @@ with open ("data.json", "r") as f:
 
 client = commands.Bot(command_prefix=prefix)
 
-@client.command()
-async def ping(ctx):
-    await ctx.send("pong")
+#Launch
 
 @client.event
 async def on_ready():
@@ -21,5 +20,79 @@ async def on_ready():
         
     with open(f"./data.json","w") as t:
         json.dump(data,t)
+
+#Load
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'{extension} command loaded.')
+    print(f'{ctx.author} used load')
+
+@load.error
+async def load_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(title="Load", description="Vous n'avez pas la permission d'éxecuter cette commande.", color=discord.Color.from_rgb(17, 100, 20))
+        await ctx.send(embed=embed)
+        
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'{extension} command unloaded.')
+    print(f'{ctx.author} used unload')
+
+@unload.error
+async def unload_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(title="Unload", description="Vous n'avez pas la permission d'éxecuter cette commande.", color=discord.Color.from_rgb(17, 100, 20))
+        await ctx.send(embed=embed)
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def reload(ctx, extension):
+    if extension == "all":
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                client.unload_extension(f'cogs.{filename[:-3]}')
+                await ctx.send(f'{filename[:-3]} unloaded')
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                client.load_extension(f'cogs.{filename[:-3]}')
+                await ctx.send(f'{filename[:-3]} reloaded')
+        print(f"{ctx.author} used reload all")
+        await ctx.send("Toutes les commandes ont été reload.")
+        
+    else:
+        print(f'{ctx.author} used reload')
+        client.unload_extension(f'cogs.{    extension}')
+        await ctx.send(f'{extension} command unloaded.')
+        client.load_extension(f'cogs.{extension}')
+        await ctx.send(f'{extension} command reloaded.')
+        print(f'{ctx.author} used reload')
+
+#commands
+
+@client.command()
+async def setprefix(ctx, prefix):
+    with open (f"./data.json", "r") as f:
+        data = json.load(f)
+    data["Prefix"] = f"{prefix}"
+    with open (f"./data.json", "w") as t:
+        json.dump(data, t)
+    await ctx.send(f'Prefix définie sur {prefix} au prochain redémarrage')
+
+class NewHelpName(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        embed = discord.Embed(title="Help", description="Toutes les commandes du client discord", color=0x992d22)
+        embed.add_field(name=f"{prefix}help", value="Affiche ce message.", inline=True)
+        await destination.send(embed=embed)
+client.help_command = NewHelpName()
 
 client.run(token)
