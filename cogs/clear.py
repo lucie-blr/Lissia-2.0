175955977622b9a0bb3b416382c6f1797fb107ec
LoliChann <l.otaku.roux@gmail.com>
2022-04-05 13:56:40 +0200

    clear /command

--- cogs/clear.py
+++ cogs/clear.py
@@ -6,19 +6,19 @@ import json
 class Clear(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    with open (f"data.json", "r") as t:
+        data2 = json.load(t)
+        servlist = data2["servlist"]
 
-    @commands.command(aliases=['purge'])
+    @commands.slash_command(guild_ids = servlist, name = "clear", description = "clear messages")
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=10):
         with open (f"data.json", "r") as t:
                 data2 = json.load(t)
                 color = data2["color"]
-        await ctx.send(f'Supression de {amount} messages...')
-        await ctx.channel.purge(limit=amount+2)
-        embed = discord.Embed(title="Clear", description=f"{amount} messages ont été supprimés !", color=discord.Color.from_rgb(color[0], color[1], color[2]))
-        embed.set_footer(text="Bot by LoliChann", icon_url=f"https://i.pinimg.com/564x/d5/d6/ff/d5d6ff7f3a344085dbffc4a9a34f538e.jpg")
-        await ctx.send(embed=embed, delete_after=10)
-        author = ctx.message.author
+        await ctx.channel.purge(limit=int(amount)+2)
+        author = ctx.author
         with open (f"./{ctx.guild.id}/data.json", "r") as f:
             data = json.load(f)
             log = data["log"]
@@ -29,6 +29,9 @@ class Clear(commands.Cog):
         embed.set_footer(text="Bot by LoliChann", icon_url=f"https://i.pinimg.com/564x/d5/d6/ff/d5d6ff7f3a344085dbffc4a9a34f538e.jpg")
         await serv.send(embed=embed)
         print(f'{ctx.author} used clear')
+        embed = discord.Embed(title="Clear", description=f"{amount} messages ont été supprimés !", color=discord.Color.from_rgb(color[0], color[1], color[2]))
+        embed.set_footer(text="Bot by LoliChann", icon_url=f"https://i.pinimg.com/564x/d5/d6/ff/d5d6ff7f3a344085dbffc4a9a34f538e.jpg")
+        await ctx.respond(embed=embed, ephemeral=False, delete_after=10)
 
     @clear.error
     async def clear_error(self, ctx, error):
@@ -38,7 +41,7 @@ class Clear(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             embed = discord.Embed(title="Clear", description="Vous n'avez pas la permission d'exécuter cette permission", color=discord.Color.from_rgb(color[0], color[1], color[2]))
             embed.set_footer(text="Bot by LoliChann", icon_url=f"https://i.pinimg.com/564x/d5/d6/ff/d5d6ff7f3a344085dbffc4a9a34f538e.jpg")
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed, ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Clear(bot))
--- main.py
+++ main.py
@@ -17,6 +17,12 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(f'{prefix}help | Vroum vroum'))
     print(f"Logged in as {client.user}")
     data[f'morpion'] = "False"
+    glist = []
+    
+    for guild in client.guilds:
+        glist.append(guild.id)
+    
+    data["servlist"] = glist
         
     with open(f"./data.json","w") as t:
         json.dump(data,t)
